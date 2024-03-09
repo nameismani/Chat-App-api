ccd2575010b1feb308f5899504f9097f42acf47a
nameismani <mm2729025@gmail.com>
2024-03-10 00:50:10 +0530

    for render

--- server.js
+++ server.js
@@ -55,8 +55,12 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://mern-live-chat-app.netlify.app/",
-    credentials: true,
+    // origin: [
+    //   "http://localhost:5173",
+    //   "https://mern-live-chat-app.netlify.app/",
+    // ],
+    origin: "http://localhost:5173",
+    // credentials: true,
   },
 });
 
